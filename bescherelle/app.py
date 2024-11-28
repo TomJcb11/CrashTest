228b2193ad50a2f9b79a2f1fe3a452a0f382ca58
TomJcb11 <thomasjacob454@gmail.com>
2024-11-28 15:29:45 +0100

    a priori bescherelle fini

--- bescherelle/app.py
+++ bescherelle/app.py
@@ -1,45 +1,92 @@
-import streamlit as st 
+import streamlit as st
+import pandas as pd
 
-st.header("Streamlit_ Bescherelle")
-tab1, tab2, tab3 = st.tabs(["1 er groupe", "2 ème groupe", "3 ème groupe"])
+csv_file = "./DB/verbList.csv"
 
-with tab1:
-    st.text_input("Rechercher un verbe", key="input_tab1")
-    st.markdown("""
-                ### Conjugaison des verbes du 1-er groupe
-                Les verbes du premier groupe sont les verbes qui se terminent par e, r. Exemples : 
-                - chanter, 
-                - regarder, 
-                - crier.
-                - etc,...
-               """)
-    
-    
+# Dictionnaires de correspondance pour les modes et les temps
+mode_dict = {
+    "inf": "infinitif",
+    "ind": "indicatif",
+    "sub": "subjonctif",
+    "par": "participe",
+    "imp": "impératif",
+    "cnd": "conditionnel",
+}
 
-with tab2:
-    st.markdown("""
-                ### Conjugaison des verbes du 2-ème groupe
-                Les verbes du deuxième groupe sont les verbes qui se terminent par ir. Exemples : 
-                - finir, 
-                - choisir""")
-    st.text_input("Rechercher un verbe", key="input_tab2")
+tense_dict = {
+    "pre": "présent",
+    "imp": "imparfait",
+    "pas": "passé",
+    "fut": "futur",
+}
 
-with tab3:
-    st.text_input("Rechercher un verbe", key="input_tab3")
-    st.markdown("""
-                ### Conjugaison des verbes du 3-ème groupe
-                Les verbes du troisième groupe sont tous les autres verbes. Ils sont très nombreux et très variés. Par exemple: 
-                 - aller
-                 - avoir
-                 - être
-                 - faire 
-                 - dire
-                 - pouvoir 
-                 - vouloir
-                 - savoir
-                 - etc...
-                 """ )
-    
-    st.text("ici globalement force pour implémenter ca")
-    
-    
+
+# Lire le fichier csv avec le bon séparateur
+verbs = pd.read_csv(csv_file, sep=";")
+
+# Créer un DataFrame à partir des données lues
+df = pd.DataFrame(verbs)
+
+
+# Fonction pour obtenir les verbes par lemme
+def get_verbs_by_lemme(df, lemme):
+    return df[df["lemme"] == lemme]
+
+
+# Fonction pour trier les conjugaisons
+def sort_conjugations(conjugations):
+    order = {"1s": 0, "2s": 1, "3s": 2, "1p": 3, "2p": 4, "3p": 5}
+    return sorted(conjugations, key=lambda x: order.get(x.split()[-1].strip("()"), 6))
+
+
+# Fonction pour afficher la conjugaison en fonction de la grammaire infover
+def display_conjugation(filtered_df):
+    conjugation_dict = {}
+    for index, row in filtered_df.iterrows():
+        infover = row["infover"].split(";")
+        for info in infover:
+            if info and len(info.split(":")) == 3:
+                mode, tense, person = info.split(":")
+                mode_full = mode_dict.get(mode, mode)
+                tense_full = tense_dict.get(tense, tense)
+                conjugation = f"{row['Word']} ({person})"
+                if mode_full not in conjugation_dict:
+                    conjugation_dict[mode_full] = {}
+                if tense_full not in conjugation_dict[mode_full]:
+                    conjugation_dict[mode_full][tense_full] = []
+                conjugation_dict[mode_full][tense_full].append(conjugation)
+
+    # Créer un dictionnaire de DataFrames pour chaque mode/temps
+    dataframes = {}
+    for mode, tenses in conjugation_dict.items():
+        for tense, conjugations in tenses.items():
+            sorted_conjugations = sort_conjugations(conjugations)
+            data = {"Conjugation": sorted_conjugations}
+            df_mode_tense = pd.DataFrame(data)
+            dataframes[f"{mode} - {tense}"] = df_mode_tense
+
+    return dataframes
+
+
+# récupérer tous les verbes uniques
+def retrieve_all_verbs():
+    return df["lemme"].unique()
+
+
+all_verbs = retrieve_all_verbs()
+
+# Création de la page Streamlit
+st.header("Streamlit Bescherelle")
+
+chosen_verb = st.selectbox(
+    label="Choisissez le verbe dont vous souhaitez la conjugaison", options=all_verbs
+)
+
+if chosen_verb:
+    filtered_df = get_verbs_by_lemme(df, chosen_verb)
+    conjugation_dfs = display_conjugation(filtered_df)
+
+    st.subheader(f"Lemme: {chosen_verb}")
+    for key, df_mode_tense in conjugation_dfs.items():
+        st.subheader(key)
+        st.dataframe(df_mode_tense)
